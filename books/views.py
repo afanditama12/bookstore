9f951dc8d7398ddf578a6eb63020aed1de586dbe
Affandi Pratama <60037959+afanditama12@users.noreply.github.com>
2022-11-04 15:01:20 +0700

    repository project bookstore dengan framework python django

---
+++ books/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+# this is a view for listing all the books
+def home(request):
+    return HttpResponse('Home Page')
+
+# this is a view for listing a single book,it will take id as an argument
+def book_detail(request, id):
+    return HttpResponse('Book Detail')
+
+# this is a list for adding a book
+def add_book(request):
+    return HttpResponse('Add Book')
+
+# this is a view for editing the book's info
+def edit_book(request):
+    return HttpResponse('Edit Book')
+
+# this is a view for deleting a book,it will take id as an argument
+def delete_book(request, id):
+    return HttpResponse('Delete Book')
