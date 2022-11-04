34c6ef417938879f29fba0e74786e5b013a33ba7
Affandi Pratama <60037959+afanditama12@users.noreply.github.com>
2022-11-05 05:07:36 +0700

    buat file forms.py untuk membuat model form

---
+++ books/forms.py
@@ -0,0 +1,22 @@
+from .models import Book
+# import ModelForm
+from django.forms import ModelForm
+# import forms
+from django import forms
+
+# declaring the ModelForm
+class EditBookForm(ModelForm):
+	
+	class Meta:
+        # the Model from which the form will inherit from
+        model = Book
+        # the fields we want from the Model
+        fields = '__all__'
+        # styling the form with bootstrap classes
+        widgets = {
+             'title': forms.TextInput(attrs={'class': 'form-control'}),
+             'author': forms.TextInput(attrs={'class': 'form-control'}),
+             'price': forms.TextInput(attrs={'class': 'form-control'}),
+             'isbn': forms.TextInput(attrs={'class': 'form-control'}),
+             'image': forms.TextInput(attrs={'class': 'form-control'}),
+        }
